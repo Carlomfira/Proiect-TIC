5e1d0ee346344ee6511b69c0e56ac55bf652891b
Carlomfira <88661929+Carlomfira@users.noreply.github.com>
2023-07-05 15:14:11 +0300

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+// module.exports = {
+//   devServer: {
+//     port: 8080,
+//     host: '18.211.47.55'
+//   }
+// }
